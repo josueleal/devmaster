b43fad25af930cdf98bf95d17ba3eb45b66fbd1b
Josué Leal <jpleal.net@gmail.com>
2020-02-22 22:42:04 -0300

    Adicionando collapse ao projeto

--- index.php
+++ index.php
@@ -46,6 +46,20 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam aut corporis cumque dolor eaque eligendi fuga iure iusto, magni minus, molestiae neque nobis non odit quia, tenetur ullam voluptatibus?</p>
             </div>
         </div>
+
+        <p>
+            <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                Link com href
+            </a>
+            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                Botão com data-target
+            </button>
+        </p>
+        <div class="collapse" id="collapseExample">
+            <div class="card card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+            </div>
+        </div>
     </div>
 
 
